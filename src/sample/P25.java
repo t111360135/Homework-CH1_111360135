5096e1764963b4a7c39f7ae0a7d7e753611d6677
111360135 <t111360135@ntut.org.tw>
2024-09-28 16:20:45 +0800

    111360135

---
+++ src/sample/P25.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class P25 {
+
+	public static void main(String[] args) {
+		int num;
+		
+		num = 3;
+		System.out.println("變數num的值是"+num);
+
+	}
+
+}
---
+++ src/sample/P9.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class P9 {
+
+	public static void main(String[] args) {
+		System.out.print("歡迎使用Java！");
+        System.out.print("開始使用Java吧！");
+	}
+
+}
